d9611e825ac9eb664a8e54f098a2e0eeeaec2bf1
CAEUser <CAEAddress@gmail.com>
2015-08-26 10:09:22 +0200

    Generated frontend component 0.1

---
+++ js/applicationScript.js
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2015 Advanced Community Information Systems (ACIS) Group, Chair
+ * of Computer Science 5 (Databases & Information Systems), RWTH Aachen
+ * University, Germany All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 
+ * Neither the name of the ACIS Group nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+var client;
+
+var init = function() {
+  
+  var iwcCallback = function(intent) {
+    // define your reactions on incoming iwc events here
+    console.log(intent);
+    if (intent.action == "RETURN_GRAPH") {
+      storeGraph(intent.data);
+    }
+  };
+  
+  client = new Las2peerWidgetLibrary("http://localhost:8081/graphs", iwcCallback);
+  
+
+  $('#storeButton').on('click', function() {
+    sendStoreGraphIntent();
+  })
+  $('#graphTable').on('click', function() {
+    var id = null;
+    loadGraph(id);
+  })
+  $('#newButton').on('click', function() {
+    sendLoadEmptyGraph();
+  })
+}
+
+
+// getGraphs
+var getGraphs = function(){
+  client.sendRequest("GET", "", "", "", {},
+  function(data, type) {
+    console.log(data);
+  },
+  function(error) {
+    console.log(error);
+  });
+$("#graphTable").html("Upated Element");
+}
+
+
+// loadGraph
+var loadGraph = function(id){
+  client.sendRequest("GET", "/id", "", "", {},
+  function(data, type) {
+    console.log(data);
+  },
+  function(error) {
+    console.log(error);
+  });
+$("#descriptionInput").html("Upated Element");
+  var graph = "initialized";
+  client.sendIntent("LOAD_GRAPH", graph);
+}
+
+
+// sendStoreGraphIntent
+var sendStoreGraphIntent = function(){
+  var noData = "initialized";
+  client.sendIntent("STORE_GRAPH", noData);
+}
+
+
+// storeGraph
+var storeGraph = function(graph){
+  var graph = null;
+  client.sendRequest("POST", "", graph, "application/json", {},
+  function(data, type) {
+    console.log(data);
+  },
+  function(error) {
+    console.log(error);
+  });
+}
+
+
+// sendLoadEmptyGraph
+var sendLoadEmptyGraph = function(){
+  var graph = "initialized";
+  client.sendIntent("LOAD_GRAPH", graph);
+}
+
+
+$(document).ready(function() {
+  init();
+});
